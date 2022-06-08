7bb4aba206c6db4c359533437f2f88bbc983d81e
Sergei Babrakov <s.a.babrakov@gmail.com>
2022-06-08 15:14:25 +0300

    Transcript object.

---
+++ src/main/java/Transcript.java
@@ -0,0 +1,40 @@
+public class Transcript {
+
+    String audio_url;
+    String id;
+    String status;
+    String text;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAudio_url() {
+        return audio_url;
+    }
+
+    public void setAudio_url(String audio_url) {
+        this.audio_url = audio_url;
+    }
+}
